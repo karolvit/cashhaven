9a9186353c783497b0dbf63689f48846a4dbce6b
karolvit <karolinavit2@gmail.com>
2025-08-03 00:31:24 -0300

    Criação do parametro do PDV

--- Frontend/src/paginas/Parametros.jsx
+++ Frontend/src/paginas/Parametros.jsx
@@ -98,6 +98,49 @@ const SwitchContainer = styled.div`
   align-items: center;
   gap: 10px;
 `;
+
+const SwitchLabel = styled.label`
+  position: relative;
+  display: inline-block;
+  width: 50px;
+  height: 26px;
+`;
+const SwitchInput = styled.input`
+  opacity: 0;
+  width: 0;
+  height: 0;
+
+  &:checked + span {
+    background-color: #73287d;
+  }
+
+  &:checked + span:before {
+    transform: translateX(24px);
+  }
+`;
+const SwitchSlider = styled.span`
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  border-radius: 26px;
+  transition: 0.4s;
+
+  &:before {
+    position: absolute;
+    content: "";
+    height: 20px;
+    width: 20px;
+    left: 3px;
+    bottom: 3px;
+    background-color: white;
+    border-radius: 50%;
+    transition: 0.4s;
+  }
+`;
 const BotaoSalvar = styled.button`
   padding: 0.7rem 2rem;
   background: #73287d;
@@ -117,7 +160,7 @@ const BotaoSalvar = styled.button`
 
 const Parametros = () => {
   const [abaAtual, setAbaAtual] = useState("empresa");
-  const [vendaManual, setVendaManual] = useState(false);
+  const [vendaManual, setVendaManual] = useState("0.00");
   const [empresa, setEmpresa] = useState(null);
   const [bairro, setBairro] = useState("")
   const [cidade, setCidade] = useState("")
@@ -129,8 +172,8 @@ const Parametros = () => {
   const [ipPrincipal, setIpPrincipal] = useState("")
   const [modeloImpressora, setModeloImpressora] = useState("")
   const [modalAberta, setModalAberta] = useState(false);
-  const [sdmin, setSdmin] = useState("");
-  const [point, setPoint] = useState("");
+  const [sdmin, setSdmin] = useState("0.00");
+  const [point, setPoint] = useState("0.00");
 
   useEffect(() => {
       const carregandoDadosEmpresa = async () => {
@@ -172,16 +215,11 @@ const Parametros = () => {
     useEffect(() => {
         const carregarParametros = async () => {
           try {
-            const token = localStorage.getItem("token");
-            const res = await apiAcai.get("param/all", {
-              headers: {
-                Authorization: `Bearer ${token}`,
-              },
-            });
+            const res = await apiAcai.get("param/all");
     
-            setSdmin(res.data.message[5].valor);
-            setPoint(res.data.message[2].valor);
-            setVendaManual(res.data.message[9].bit);
+            setSdmin(formatarParaDecimal(res.data.message[5].valor.toString()));
+            setPoint(formatarParaDecimal(res.data.message[2].valor.toString()));
+            setVendaManual(res.data.message[9].valor);
           } catch (error) {
             console.log("Erro", error);
           }
@@ -189,7 +227,41 @@ const Parametros = () => {
         carregarParametros();
       }, []);
 
+      const atualizarParametros = async (e) => {
+        e.preventDefault();
+        try {
+        
+          const payload = [
+            { id: 6, valor: sdmin, bit: 1},
+            { id: 3, valor: point,bit: 1 },
+            { id: 10, valor: vendaManual, bit: 1},
+          ];
       
+          const res = await apiAcai.put("/param/update", payload)
+      
+          if (res.status === 200) { 
+            //window.location.reload();
+          }
+        } catch (error) {
+          console.error("Erro ao atualizar parâmetros:", error);
+        }
+      };
+      function formatarParaDecimal(valor) {
+        const numeros = valor.replace(/\D/g, "");
+      
+        if (!numeros) return "0.00";
+      
+        if (numeros.length === 1) return "0.0" + numeros;
+        if (numeros.length === 2) return "0." + numeros;
+      
+        const parteInteira = numeros.slice(0, -2);
+        const parteDecimal = numeros.slice(-2);
+      
+        return parteInteira + "." + parteDecimal;
+      }
+      function desformatarDecimal(valor) {
+        return parseFloat(valor);
+      }
   return (
     <>
       <GlobalStyle />
@@ -246,28 +318,37 @@ const Parametros = () => {
 
         {abaAtual === "pdv" && (
           <Form>
-            <Linha>
-              <label>Saldo mínimo para resgate</label>
-              <input value={sdmin} />
-            </Linha>
-            <Linha>
-              <label>Percentual cashback</label>
-              <input value={point} />
-            </Linha>
+    <Linha>
+      <label>Saldo mínimo para resgate</label>
+      <input
+        value={sdmin}
+        onChange={(e) => setSdmin(formatarParaDecimal(e.target.value))}
+      />
+    </Linha>
+    <Linha>
+            <label>Percentual cashback</label>
+      <input
+      value={point}
+       onChange={(e) => setPoint(formatarParaDecimal(e.target.value))}
+      />
+       </Linha>
             <Linha>
               <label>Desativar/Ativar venda manual</label>
               <SwitchContainer>
-                <input
-                  type="checkbox"
-                  checked={vendaManual}
-                  onChange={() => setVendaManual(!vendaManual)}
-                />
-                <span>{vendaManual ? "Ativo" : "Inativo"}</span>
+                <SwitchLabel>
+                  <SwitchInput
+                    type="checkbox"
+                    checked={vendaManual === "1.00"}
+                    onChange={(e) => setVendaManual(e.target.checked ? "1.00" : "0.00")}
+                  />
+                  <SwitchSlider />
+                </SwitchLabel>
+                <span>{vendaManual === "1.00" ? "Ativo" : "Inativo"}</span>
               </SwitchContainer>
             </Linha>
             <div style={{ gridColumn: "1 / -1", display: "flex", justifyContent: "center" }}>
-  <BotaoSalvar>Salvar</BotaoSalvar>
-</div>
+                  <BotaoSalvar onClick={atualizarParametros}> Salvar</BotaoSalvar>
+            </div>
 
           </Form>
         )}
